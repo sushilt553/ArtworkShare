46f2ec40fa5df11690bc3c557b26409e3c483244
Sushil <sushilt553@gmail.com>
2020-06-24 14:18:26 -0400

    Artwork association complete

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,4 +2,8 @@ class Artwork < ApplicationRecord
 
     validates :title, :image_url, presence: true
     validates :title, uniqueness: {scope: :artist_id}
+
+    has_many :artwork_shares
+    belongs_to :artist, class_name: :User
+    has_many :shared_viewers, through: :artwork_shares
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true
+
+    has_many :artworks
+    has_many :artwork_shares
+    has_many :shared_artworks, through: :artwork_shares
 end
